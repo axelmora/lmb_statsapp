96f18d706f648da171f0f7b4ea30e20e89adb1f0
axel.asmo@gmail.com <axel.asmo@gmail.com>
2018-04-13 23:18:33 -0500

    google sheets integration

--- lmb_stats/app.R
+++ lmb_stats/app.R
@@ -126,10 +126,9 @@ ui <- dashboardPage(
 
 server <- function(input, output) { 
 
-
-  
   output$standings <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"
+    LMBts <- read.csv(url(ts.u))
     lmb.ts <- xts(LMBts[,-1], order.by = as.Date(LMBts$DATE, "%Y-%m-%d"))
     
     dygraph(lmb.ts, main = "2018 LMB standings", ylab = "Win/Loss percentage") %>%
@@ -159,10 +158,11 @@ server <- function(input, output) {
   })  
   
   output$norte <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
-    norte <- c(2,3,4,5,7,8,11,12,15)
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"
+    LMBts <- read.csv(url(ts.u))
+    norte <- c(1,2,3,4,6,7,10,11,14)
     LMBts_nte <- LMBts[norte]
-    lmb.tsN <- xts(LMBts_nte[,-1], order.by = as.Date(LMBts_nte$DATE, "%Y-%m-%d"))
+    lmb.tsN <- xts(LMBts_nte[,2:9], order.by = as.Date(LMBts_nte$DATE, "%Y-%m-%d"))
     dygraph(lmb.tsN, main = "2018 LMB standings", ylab = "Win/Loss percentage") %>%
       dySeries("lar", label = "Tecolotes") %>%
       dySeries("vaq", label = "Vaqueros") %>%
@@ -181,10 +181,11 @@ server <- function(input, output) {
   })
   
   output$sur <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
-    sur <- c(2,6,9,10,13,14,16,17,18)
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"
+    LMBts <- read.csv(url(ts.u))
+    sur <- c(1,5,8,9,12,13,15,16,17)
     LMBts_sur <- LMBts[sur]
-    lmb.tsS <- xts(LMBts_sur[,-1], order.by = as.Date(LMBts_sur$DATE, "%Y-%m-%d"))
+    lmb.tsS <- xts(LMBts_sur[,2:9], order.by = as.Date(LMBts_sur$DATE, "%Y-%m-%d"))
     dygraph(lmb.tsS, main = "2018 LMB standings", ylab = "Win/Loss percentage") %>%
       dySeries("leo", label = "Bravos") %>%
       dySeries("mxo", label = "Diablos Rojos") %>%
@@ -202,7 +203,8 @@ server <- function(input, output) {
   })
   
   output$ags <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"
+    LMBts <- read.csv(url(ts.u))
     aguts <- rename(select(LMBts,DATE,agu),WL_AVG = agu, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     agu.ts <- xts(aguts$WL_AVG, order.by = as.Date(aguts$DATE, "%Y-%m-%d"))
@@ -214,7 +216,8 @@ server <- function(input, output) {
   })
   
   output$cam <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     camts <- rename(select(LMBts,DATE,cam),WL_AVG = cam, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     cam.ts <- xts(camts$WL_AVG, order.by = as.Date(camts$DATE, "%Y-%m-%d"))
@@ -226,7 +229,8 @@ server <- function(input, output) {
   })
   
   output$dur <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     durts <- rename(select(LMBts,DATE,dur),WL_AVG = dur, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     dur.ts <- xts(durts$WL_AVG, order.by = as.Date(durts$DATE, "%Y-%m-%d"))
@@ -238,7 +242,8 @@ server <- function(input, output) {
   })
   
   output$leo <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     leots <- rename(select(LMBts,DATE,leo),WL_AVG = leo, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     leo.ts <- xts(leots$WL_AVG, order.by = as.Date(leots$DATE, "%Y-%m-%d"))
@@ -250,7 +255,8 @@ server <- function(input, output) {
   })
   
   output$mty <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     mtyts <- rename(select(LMBts,DATE,mty),WL_AVG = mty, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     mty.ts <- xts(mtyts$WL_AVG, order.by = as.Date(mtyts$DATE, "%Y-%m-%d"))
@@ -262,7 +268,8 @@ server <- function(input, output) {
   })
   
   output$mva <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     mvats <- rename(select(LMBts,DATE,mva),WL_AVG = mva, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     mva.ts <- xts(mvats$WL_AVG, order.by = as.Date(mvats$DATE, "%Y-%m-%d"))
@@ -274,7 +281,8 @@ server <- function(input, output) {
   })
   
   output$mex <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     mxots <- rename(select(LMBts,DATE,mxo),WL_AVG = mxo, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     mxo.ts <- xts(mxots$WL_AVG, order.by = as.Date(mxots$DATE, "%Y-%m-%d"))
@@ -286,7 +294,8 @@ server <- function(input, output) {
   })
   
   output$oax <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     oaxts <- rename(select(LMBts,DATE,oax),WL_AVG = oax, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     oax.ts <- xts(oaxts$WL_AVG, order.by = as.Date(oaxts$DATE, "%Y-%m-%d"))
@@ -298,7 +307,8 @@ server <- function(input, output) {
   })
   
   output$pue <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     puets <- rename(select(LMBts,DATE,pue),WL_AVG = pue, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     pue.ts <- xts(puets$WL_AVG, order.by = as.Date(puets$DATE, "%Y-%m-%d"))
@@ -310,7 +320,8 @@ server <- function(input, output) {
   })
   
   output$qui <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     quits <- rename(select(LMBts,DATE,qui),WL_AVG = qui, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     qui.ts <- xts(quits$WL_AVG, order.by = as.Date(quits$DATE, "%Y-%m-%d"))
@@ -322,7 +333,8 @@ server <- function(input, output) {
   })
   
   output$sal <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     sltts <- rename(select(LMBts,DATE,slt),WL_AVG = slt, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     slt.ts <- xts(sltts$WL_AVG, order.by = as.Date(sltts$DATE, "%Y-%m-%d"))
@@ -334,7 +346,8 @@ server <- function(input, output) {
   })
   
   output$tab <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     tabts <- rename(select(LMBts,DATE,tab),WL_AVG = tab, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     tab.ts <- xts(tabts$WL_AVG, order.by = as.Date(tabts$DATE, "%Y-%m-%d"))
@@ -346,7 +359,8 @@ server <- function(input, output) {
   })
   
   output$tij <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     tijts <- rename(select(LMBts,DATE,tij),WL_AVG = tij, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     tij.ts <- xts(tijts$WL_AVG, order.by = as.Date(tijts$DATE, "%Y-%m-%d"))
@@ -358,7 +372,8 @@ server <- function(input, output) {
   })
   
   output$lag <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     lagts <- rename(select(LMBts,DATE,vaq),WL_AVG = vaq, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     lag.ts <- xts(lagts$WL_AVG, order.by = as.Date(lagts$DATE, "%Y-%m-%d"))
@@ -370,7 +385,8 @@ server <- function(input, output) {
   })
   
   output$lar <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     larts <- rename(select(LMBts,DATE,lar),WL_AVG = lar, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     lar.ts <- xts(larts$WL_AVG, order.by = as.Date(larts$DATE, "%Y-%m-%d"))
@@ -382,7 +398,8 @@ server <- function(input, output) {
   })
   
   output$yuc <- renderDygraph({
-    LMBts <- read.csv("LMBts.csv")
+    ts.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCuLmj4gmK_L06zKI9nyc_EoiRNqVwl10H4CSDfGOsIjWHLok7OBCg_RGMA57C5sLjq1twOQMb488T/pub?gid=948498967&single=true&output=csv"     
+    LMBts <- read.csv(url(ts.u))
     yucts <- rename(select(LMBts,DATE,yuc),WL_AVG = yuc, DATE = DATE)
     #lmb_exp <- read.csv("LMB2018stan")
     yuc.ts <- xts(yucts$WL_AVG, order.by = as.Date(yucts$DATE, "%Y-%m-%d"))
@@ -394,7 +411,8 @@ server <- function(input, output) {
   })
   
   output$time <- renderDygraph({
-    LMBatt_time <- read.csv("LMB2018.csv")
+    att_time.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vStbs_L96FBpcZKd3ZPm0OztsT8g9xYK95ZR67-UGsG0aYYxohtFmrWGhav4pqpn0bMZ8REBUMehiGi/pub?gid=1306250727&single=true&output=csv"
+    LMBatt_time <- read.csv(url(att_time.u))
     att_time <- LMBatt_time %>%
       group_by(DATE) %>% summarise(
         ATT = mean(ATT),
@@ -406,7 +424,8 @@ server <- function(input, output) {
   })
   
   output$att <- renderDygraph({
-    LMBatt_time <- read.csv("LMB2018.csv")
+    att_time.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vStbs_L96FBpcZKd3ZPm0OztsT8g9xYK95ZR67-UGsG0aYYxohtFmrWGhav4pqpn0bMZ8REBUMehiGi/pub?gid=1306250727&single=true&output=csv"
+    LMBatt_time <- read.csv(url(att_time.u))
     att_time <- LMBatt_time %>%
       group_by(DATE) %>% summarise(
         ATT = mean(ATT),
@@ -418,34 +437,38 @@ server <- function(input, output) {
   })
   
   output$stan_nte <- renderDataTable({
-    stan.nte <- read.csv("LMB2018_stan_N.csv")
-    stan.nte = stan.nte[,-1]
+    stan.nte.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSqxkxMkXPDti1hUWIyn7ikmuAkQG-o1RYhqqmrh3sGyGcRvl9TxN-3cNlKsWFsrIihsN6onrmsUFJu/pub?gid=2113179783&single=true&output=csv"
+    stan.nte <- read.csv(url(stan.nte.u))
+    #stan.nte = stan.nte[,-1]
     stan.nte <- arrange(stan.nte, desc(PCT), TEAM)
     datatable(stan.nte, options = list(paging = FALSE, searching = FALSE)) %>%
       formatRound(columns = c('PCT','RperG','RAperG','RunsRatio','PCTexp','WinRatio'), digits = 3)
   })
   
   output$stan_sur <- renderDataTable({
-    stan.sur <- read.csv("LMB2018_stan_S.csv")
-    stan.sur = stan.sur[,-1]
+    stan.sur.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vT8Gt45XnEaKcm47RFwAzLeq0svwGMRgRKbuJu-wqYtPuRXSooZ4XrQJprFfMStY5KjCRyjpFbv5x2a/pub?gid=742234677&single=true&output=csv"
+    stan.sur <- read.csv(url(stan.sur.u))
+    #stan.sur = stan.sur[,-1]
     stan.sur <- arrange(stan.sur, desc(PCT), TEAM)
     datatable(stan.sur, options = list(paging = FALSE, searching = FALSE)) %>%
       formatRound(columns = c('PCT','RperG','RAperG','RunsRatio','PCTexp','WinRatio'), digits = 3)
   })
   
   output$bat <- renderDataTable({
-    lmb_b <- read.csv("LMB2018_bat_bas.csv") %>%
+    lmb.b.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5mSK8ZlY1qz1Sb7VSMiMSAthNY9JY5Mp05SyXbw5CsZAZhkOrgd75UU8NAEVH5zs122dDqmzam5NL/pub?gid=93773820&single=true&output=csv"
+    lmb_b <- read.csv(url(lmb.b.u)) %>%
       arrange(desc(AVG))
-    datatable(lmb_b[,-1], extensions = 'FixedColumns',
+    datatable(lmb_b, extensions = 'FixedColumns',
               options = list(scrollX = TRUE, fixedColumns = TRUE, pageLength = 6)
             ) %>%
       formatRound(columns = c('AVG','OBP','SLG','OPS'), digits = 3)
   })
   
   output$pit <- renderDataTable({
-    lmb_p <- read.csv("LMB2018_pit_bas.csv") %>%
+    lmb.p.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZ0LlEBYTwbjyHvzhlsxD3WYBC0Dpzp79zyl2bNsXWUoyYN6Dk-4aE0zD7imwcfqstvf1Dvw1t36rl/pub?gid=203948910&single=true&output=csv"
+    lmb_p <- read.csv(url(lmb.p.u)) %>%
       arrange(desc(ERA))
-    datatable(lmb_p[,-1], extensions = 'FixedColumns',
+    datatable(lmb_p, extensions = 'FixedColumns',
               options = list(scrollX = TRUE, fixedColumns = TRUE, pageLength = 6)
     ) %>%
       formatRound(columns = c('ERA','WHIP'), digits = 2) %>%
@@ -453,18 +476,20 @@ server <- function(input, output) {
   })
   
   output$bat2 <- renderDataTable({
-    lmb_b <- read.csv("LMB2018_bat_bas.csv") %>%
+    lmb.b2.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5mSK8ZlY1qz1Sb7VSMiMSAthNY9JY5Mp05SyXbw5CsZAZhkOrgd75UU8NAEVH5zs122dDqmzam5NL/pub?gid=93773820&single=true&output=csv"
+    lmb_b <- read.csv(url(lmb.b2.u)) %>%
       arrange(desc(AVG))
-    datatable(lmb_b[,-1], extensions = 'FixedColumns',
+    datatable(lmb_b, extensions = 'FixedColumns',
               options = list(scrollX = TRUE, fixedColumns = TRUE, pageLength = 6)
     ) %>%
       formatRound(columns = c('AVG','OBP','SLG','OPS'), digits = 3)
   })
   
   output$pit2 <- renderDataTable({
-    lmb_p <- read.csv("LMB2018_pit_sab.csv") %>%
+    lmb.p2.u <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vT3nDmSldZQxj0yObqJGaqumTRUpMbJ-rN-atpxN0hIX_egbK0BQGmpiUL1Kxb-okQE-gTVKzBpjHkK/pub?gid=49577423&single=true&output=csv"
+    lmb_p <- read.csv(url(lmb.p2.u)) %>%
       arrange(desc(ERA))
-    datatable(lmb_p[,-1], extensions = 'FixedColumns',
+    datatable(lmb_p, extensions = 'FixedColumns',
               options = list(scrollX = TRUE, fixedColumns = TRUE, pageLength = 6)
     ) %>%
       formatRound(columns = c('ERA','WHIP','BB.9','K.9'), digits = 2) %>%
