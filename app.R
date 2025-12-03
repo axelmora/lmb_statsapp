5fcbd42a54d33dc1ab669b0055dabb1d7f4d0a33
axelmora <axel.asmo@gmail.com>
2025-12-03 11:00:42 -0800

    code compressed

--- app.R
+++ app.R
@@ -89,145 +89,49 @@ ui <- bs4DashPage(
     title = "LMB Analytics",
     bs4SidebarMenu(
       id = "tabs",
-      bs4SidebarMenuItem(
-        "Players",
-        tabName = "player_hit",
-        icon = icon("user")
+      bs4SidebarMenuItem("Welcome", tabName = "welcome", icon = icon("home")),
+      bs4SidebarMenuItem("Players", tabName = "player_hit", icon = icon("user")),
+      bs4SidebarMenuItem("Teams", tabName = "teams_stats", icon = icon("users"), startExpanded = TRUE,
+        bs4SidebarMenuSubItem("Team Stats",tabName = "team_stats",icon = icon("table")),
+        bs4SidebarMenuSubItem("Rosters",tabName = "team_rosters",icon = icon("address-book")),
+        bs4SidebarMenuSubItem("Transactions",tabName = "team_transactions",icon = icon("exchange-alt"))
       ),
       bs4SidebarMenuItem(
-        "Teams",
-        tabName = "teams_stats",
-        icon = icon("users"),
-        startExpanded = TRUE,
-        bs4SidebarMenuSubItem(
-          "Team Stats",
-          tabName = "team_stats",
-          icon = icon("table")
-        ),
-        bs4SidebarMenuSubItem(
-          "Rosters",
-          tabName = "team_rosters",
-          icon = icon("address-book")
-        ),
-        bs4SidebarMenuSubItem(
-          "Transactions",
-          tabName = "team_transactions",
-          icon = icon("exchange-alt")
-        )
+        "Player Comparison",tabName = "player_comparison",icon = icon("arrow-right-arrow-left")),
+      bs4SidebarMenuItem("Game Logs",tabName = "game_logs",icon = icon("list-alt")),
+      bs4SidebarMenuItem("Standings",tabName = "standings_menu",icon = icon("trophy"),startExpanded = TRUE,
+        bs4SidebarMenuSubItem("Standings",tabName = "standings",icon = icon("list-ol")),
+        bs4SidebarMenuSubItem("League Standing",tabName = "league_standing",icon = icon("globe")),
+        bs4SidebarMenuSubItem("Head-to-Head",tabName = "head_to_head",icon = icon("exchange-alt")),
+        bs4SidebarMenuSubItem("Standing Evolution",tabName = "standing_evolution",icon = icon("chart-line"))
       ),
       bs4SidebarMenuItem(
-        "Player Comparison",
-        tabName = "player_comparison",
-        icon = icon("arrow-right-arrow-left")
+      "Extras",tabName = "extras_menu",icon = icon("star"),startExpanded = TRUE,
+        bs4SidebarMenuSubItem("Game Pace",tabName = "game_pace",icon = icon("clock")),
+        bs4SidebarMenuSubItem("Attendance",tabName = "attendance",icon = icon("users")),
+        bs4SidebarMenuSubItem("Guts",tabName = "guts",icon = icon("fire"))
       ),
-      bs4SidebarMenuItem(
-        "Game Logs",
-        tabName = "game_logs",
-        icon = icon("list-alt")
-      ),
-      bs4SidebarMenuItem(
-      "Standings",
-      tabName = "standings_menu",
-      icon = icon("trophy"),
-      startExpanded = TRUE,
-      bs4SidebarMenuSubItem(
-        "Standings",
-        tabName = "standings",
-        icon = icon("list-ol")
-        ),
-      bs4SidebarMenuSubItem(
-        "League Standing",
-        tabName = "league_standing",
-        icon = icon("globe")
-        ),
-      bs4SidebarMenuSubItem(
-        "Head-to-Head",
-        tabName = "head_to_head",
-        icon = icon("exchange-alt")
-        ),
-      bs4SidebarMenuSubItem(
-        "Standing Evolution",
-        tabName = "standing_evolution",
-        icon = icon("chart-line")
-        )
-      ),
-      bs4SidebarMenuItem(
-      "Extras",
-      tabName = "extras_menu",
-      icon = icon("star"),
-      startExpanded = TRUE,
-      bs4SidebarMenuSubItem(
-        "Game Pace",
-        tabName = "game_pace",
-        icon = icon("clock")
-        ),
-      bs4SidebarMenuSubItem(
-        "Attendance",
-        tabName = "attendance",
-        icon = icon("users")
-        ),
-      bs4SidebarMenuSubItem(
-        "Guts",
-        tabName = "guts",
-        icon = icon("fire")
-        )
-      ),
-      bs4SidebarMenuItem(
-        "Reports",
-        tabName = "reports",
-        icon = icon("file-pdf")
-      ),
+      bs4SidebarMenuItem("Reports",tabName = "reports",icon = icon("file-pdf")),
       bs4SidebarMenuItem("Chatbot", tabName = "chatbot", icon = icon("robot"))
     )
   ),
   body = bs4DashBody(
     bs4TabItems(
-      bs4TabItem(
-        tabName = "player_hit",
-        ui_player_stats("ui_player_stats_1")
-      ),
-      bs4TabItem(
-        tabName = "team_stats",
-        ui_team_stats("ui_team_stats_1")
-      ),
-      bs4TabItem(
-        tabName = "team_rosters",
-        ui_rosters("ui_rosters_1")
-      ),
-      bs4TabItem(
-        tabName = "team_transactions",
-        ui_transactions("ui_transactions_1")
-      ),
-      bs4TabItem(
-        tabName = "player_comparison",
-        playerComparisonUI("comparison")
-      ),
-      bs4TabItem(
-        tabName = "game_logs",
-        h3("Game Logs"),
-        ui_game_logs("ui_game_logs_1")
-      ),
+      bs4TabItem(tabName = "welcome", h3("Bienvenidos")),
+      bs4TabItem(tabName = "player_hit",ui_player_stats("ui_player_stats_1")),
+      bs4TabItem(tabName = "team_stats",ui_team_stats("ui_team_stats_1")),
+      bs4TabItem(tabName = "team_rosters",ui_rosters("ui_rosters_1")),
+      bs4TabItem(tabName = "team_transactions",ui_transactions("ui_transactions_1")),
+      bs4TabItem(tabName = "player_comparison",playerComparisonUI("comparison")),
+      bs4TabItem(tabName = "game_logs",h3("Game Logs"),ui_game_logs("ui_game_logs_1")),
       bs4TabItem(tabName = "standings", h3("Standings"), ui_zone_std("ui_zone_std_1")),
       bs4TabItem(tabName = "league_standing", h3("League Standing"), ui_lg_std("ui_lg_std_1")),
       bs4TabItem(tabName = "head_to_head", h3("Head-to-Head"), ui_h2h("ui_h2h_1")),
       bs4TabItem(tabName = "standing_evolution", h3("Standing Evolution"), ui_std_evol("ui_std_evol_1")),
-      bs4TabItem(
-        tabName = "game_pace",
-        ui_game_pace("ui_game_pace_1")
-      ),
-      bs4TabItem(
-        tabName = "attendance",
-        ui_att("ui_att_1")
-      ),
-      bs4TabItem(
-        tabName = "guts", 
-        ui_guts("ui_guts_1")
-      ),
-      bs4TabItem(
-        tabName = "reports",
-        h3("Custom Reports"),
-        ui_reports("ui_reports_1")
-      ),
+      bs4TabItem(tabName = "game_pace",ui_game_pace("ui_game_pace_1")),
+      bs4TabItem(tabName = "attendance",ui_att("ui_att_1")),
+      bs4TabItem(tabName = "guts", ui_guts("ui_guts_1")),
+      bs4TabItem(tabName = "reports",h3("Custom Reports"),ui_reports("ui_reports_1")),
       bs4TabItem(tabName = "chatbot", h3("Chatbot"), chatbotUI("chatmod"))
     )
   ),
@@ -235,7 +139,7 @@ ui <- bs4DashPage(
     id = "filters",
     skin = "light",
     title = "Filters",
-    collapsed = FALSE,  # open by default (set TRUE to start closed)
+    collapsed = FALSE,
     uiOutput("team_filters"),
     uiOutput("players_filters")
   )
@@ -249,9 +153,7 @@ server <- function(input, output, session) {
   seasons <- sort(unique(team_hitting$Year))
   
   output$players_filters <- renderUI({
-    
     if (input$tabs != "player_hit") return(NULL)
-    
     tagList(
       selectInput("players_year", "Select Season:",
                   choices  = c("All", seasons),
@@ -272,10 +174,9 @@ server <- function(input, output, session) {
   filters <- teamFiltersServer("team_filters")
 
   output$team_filters <- renderUI({
-  if (!input$tabs %in% c("team_stats", "team_rosters", "team_transactions")) return(NULL)
-  teamFiltersUI("team_filters")   # <-- this renders the UI
+    if (!input$tabs %in% c("team_stats", "team_rosters", "team_transactions")) return(NULL)
+    teamFiltersUI("team_filters")   # <-- this renders the UI
   })
-  
   
   server_team_stats("ui_team_stats_1",team_hitting,team_pitching,team_fielding, filters)
   server_rosters("ui_rosters_1", rosters, filters)
